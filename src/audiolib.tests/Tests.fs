9b725aac64e8d1fea049231cb1a4de310cad75a3
John Stovin <john.d.stovin@gmail.com>
2018-04-16 09:06:25 +0100

    Generator code

--- src/audiolib.tests/Tests.fs
+++ src/audiolib.tests/Tests.fs
@@ -1,13 +1,12 @@
 module Tests
 
 open FsCheck.Xunit
-
-let sin = Seq.initInfinite (fun _ -> 0.0) |> Seq.take 10000
+open AudioLib
 
 [<Property>]
 let ``Generator output is always below 1`` () =
-     sin |> Seq.max < 1.0
+     Generator.sine 44100 440.0 |> Seq.take 10000 |> Seq.max < 1.0
 
 [<Property>]
 let ``Generator output is always above -1`` () =
-     sin |> Seq.max > -1.0
+     Generator.sine 44100 440.0 |> Seq.take 10000 |> Seq.max > -1.0
--- src/audiolib/Library.fs
+++ src/audiolib/Library.fs
@@ -1,5 +1,16 @@
 namespace AudioLib
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+open System
+
+type AudioStream = float seq
+
+module Generator =
+    let TWOPI = Math.PI * 2.0 
+
+    let generate fn sampleRate frequency : AudioStream = 
+        let gen theta = 
+            let delta = TWOPI * frequency / float sampleRate
+            Some (fn theta, (theta + delta) % TWOPI)
+        Seq.unfold gen 0.0
+
+    let sine = generate Math.Sin
