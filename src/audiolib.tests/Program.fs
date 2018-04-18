0a7f84cc5fcbe8715fe0535bbfffe7fba8ce1e60
John Stovin <john.d.stovin@gmail.com>
2018-04-18 15:39:49 +0100

    Waveforms

--- src/audiolib.tests/Program.fs
+++ src/audiolib.tests/Program.fs
@@ -2,7 +2,16 @@ open Expecto
 open AudioLib.Generator
 open FsCheck
 
-let sine1sec frequency= sine 44100 frequency |> Seq.take 44100
+let gen1sec fn frequency =
+    fn 44100.0 frequency |> Seq.take 44100
+
+let sine1sec = gen1sec sine
+
+let square1sec = gen1sec square
+
+let saw1sec = gen1sec sawtooth
+
+let tri1sec = gen1sec triangle
 
 let countZeroCrossings stream =
     stream 
@@ -16,25 +25,47 @@ let countZeroCrossings stream =
 
 type AudioFrequencies =
     static member PositiveFloat () =
-        Arb.Default.Float() |> Arb.mapFilter abs (fun t -> t >= 0.0 && t <= 25000.0)
+        Arb.Default.Float() |> Arb.mapFilter abs (fun t -> t >= 1.0 && t <= 25000.0)
 
 let config = { FsCheckConfig.defaultConfig with arbitrary = [typeof<AudioFrequencies>] }
 
 let tests = 
     testList "Generator Tests" [
-        testProperty "Generator output is always below 1" (fun frequency ->
+        testPropertyWithConfig config "Sine output is always below 1" (fun frequency ->
             sine1sec frequency |> Seq.max <= 1.0
         )
-        testProperty "Generator output is always above -1" (fun frequency ->
+        testPropertyWithConfig config "Sine output is always above -1" (fun frequency ->
             sine1sec frequency |> Seq.min >= -1.0
+        )
+        testPropertyWithConfig config "Square +ve output is always 1" (fun frequency ->
+            square1sec frequency |> Seq.max = 1.0
+        )
+        testPropertyWithConfig config "Square -ve is always -1" (fun frequency ->
+            square1sec frequency |> Seq.min = -1.0
+        )
+        testPropertyWithConfig config "Sawtooth output is always below 1" (fun frequency ->
+            saw1sec frequency |> Seq.max <= 1.0
+        )
+        testPropertyWithConfig config "Sawtooth output is always above -1" (fun frequency ->
+            saw1sec frequency |> Seq.min >= -1.0
+        )
+        testPropertyWithConfig config "Sawtooth output is always increasing" (fun frequency ->
+            let saw = saw1sec frequency
+            saw |> Seq.pairwise |> Seq.forall (fun (x,y) -> y > x || x - y > 1.99)
+        )
+        testPropertyWithConfig config "Triangle output is always below 1" (fun frequency ->
+            tri1sec frequency |> Seq.max <= 1.0
+        )
+        testPropertyWithConfig config "Triangle output is always above -1" (fun frequency ->
+            tri1sec frequency |> Seq.min >= -1.0
         )
         testPropertyWithConfig config "1 second of f Hz contains approximately 2 * f zero crossings" (fun frequency ->
             let crossings = sine1sec frequency |> countZeroCrossings 
             let expected = int (frequency * 2.0)
-            Expect.isGreaterThanOrEqual crossings (expected - 1) "Not enough zero crossings"
+            Expect.isGreaterThanOrEqual crossings (expected) "Not enough zero crossings"
             Expect.isLessThanOrEqual crossings (expected + 1) "Too many zero crossings"
         )
-        test "1 second of 0 Hz is always 0.0" {
+        test "1 second of 0 Hz does not cross zero" {
             Expect.equal (sine1sec 0.0 |> countZeroCrossings) 0 "0 Hz signal oscillates"
         }
         test "1 second of 0 Hz contains all values at 0.0" {
--- src/audiolib/Library.fs
+++ src/audiolib/Library.fs
@@ -9,8 +9,21 @@ module Generator =
 
     let generate fn sampleRate frequency : AudioStream = 
         let gen theta = 
-            let delta = TWOPI * frequency / float sampleRate
+            let delta = TWOPI * frequency / sampleRate
             Some (fn theta, (theta + delta) % TWOPI)
         Seq.unfold gen 0.0
 
     let sine = generate Math.Sin
+
+    let square = generate (fun theta -> if theta <= Math.PI then 1.0 else -1.0)
+
+    let sawtooth = generate (fun theta -> (theta / Math.PI) - 1.0)
+
+    let triangle = 
+        let triFunc theta =
+            if theta < Math.PI then
+                (2.0 * (theta / Math.PI)) - 1.0
+            else
+                (2.0 * (2.0 - (theta / Math.PI))) - 1.0
+        generate triFunc
+                
